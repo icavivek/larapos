565a47529a9d73f37213a229c8154c0b4d78f953
Vivek Patil <ica.vivek16@gmail.com>
2021-06-18 04:03:38 +0530

    Force redirect of all urls to HTTPS 4

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,5 +1,5 @@
-<script src="{{ secure_asset('plugins/jquery/jquery-3.4.1.slim.min.js') }}"></script>
-<script src="{{ secure_asset('plugins/bootstrap/popper.min.js') }}"></script>
-<script src="{{ secure_asset('plugins/bootstrap/bootstrap.min.js') }}"></script>
-<script src="{{ secure_asset('js/side_nav.js') }}"></script>
+<script src="http://laravelpos-xqlb44i2aa-uc.a.run.app/plugins/jquery/jquery-3.4.1.slim.min.js"></script>
+<script src="https://laravelpos-xqlb44i2aa-uc.a.run.app/plugins/bootstrap/popper.min.js"></script>
+<script src="https://laravelpos-xqlb44i2aa-uc.a.run.app/plugins/bootstrap/popper.min.js"></script>
+<script src="https://laravelpos-xqlb44i2aa-uc.a.run.app/js/side_nav.js"></script>
 <script src="{{ mix('js/app.js') }}"></script>
