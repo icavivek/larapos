28739cd87f79ef063e5f7cf295e9ab41446eae7d
Vivek Patil <ica.vivek16@gmail.com>
2021-06-18 08:15:53 +0530

    Force redirect of all urls to HTTPS 17

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\URL;
+
 use Validator;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        // URL::forceScheme('https');
+
         //Add this custom validation rule.
         Validator::extend('alpha_spaces', function ($attribute, $value) {
 
@@ -27,13 +27,7 @@ class AppServiceProvider extends ServiceProvider
 
         });
 
-        If (env('APP_ENV') !== 'local') {
-            URL::forceScheme('https');
-        }
-
-
-       
-
+     
     }
 
     /**
